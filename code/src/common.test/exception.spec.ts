b780d995cb3dbdfe283560bfa7d83c428aef5b6c
Bibby <bibbynet@gmail.com>
2016-05-05 09:30:00 +0800

    add exception test

---
+++ code/src/common.test/exception.spec.ts
@@ -0,0 +1,86 @@
+/// <reference path="../../typings/main.d.ts" />
+
+import {assert} from "chai";
+import * as sinon from "sinon";
+import * as proxyquire from "proxyquire";
+import {Foo} from "../common/foo";
+import {Bar} from "../common/bar";
+
+describe("server side test => exception", () => {
+
+    var sandbox: Sinon.SinonSandbox;
+
+    beforeEach((done: MochaDone) => {
+
+        sandbox = sinon.sandbox.create();
+        //mockgoose.reset();
+        done();
+
+    });
+
+    afterEach((done: MochaDone) => {
+
+        sandbox.restore();
+        //mockgoose.reset();
+        done();
+
+    });
+
+    it("test, exception", () => {
+
+        var expected = "xxxxx";
+
+        try {
+
+            throw "xxxxx";
+
+        } catch (ex) {
+
+            var actual = ex;
+            assert.equal(actual, expected);
+
+        }
+    });
+
+    it("test, exception from promise", async () => {
+
+        var expected = "xxxxx1";
+
+        var f = () => {
+
+            var p = new Promise<void>((resolve, reject) => {
+
+                setTimeout(() => {
+
+                    try {
+
+                        throw "xxxxx1";
+
+                    } catch (ex) {
+
+                        reject(ex);
+
+                    }
+
+                }, 500);
+
+            });
+            return p;
+
+        }
+
+        try {
+
+            await f();
+
+        } catch (ex) {
+
+            var actual =ex;
+            assert.equal(actual, expected);
+
+        }
+
+    });
+
+
+});
