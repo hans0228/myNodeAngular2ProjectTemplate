58eb5a7c5bae30d5d2e66ef03101563c88f80bab
Bibby Chung <bibbynet@gmail.com>
2016-06-15 17:12:29 +0800

    fix errors for master merge

--- code/src/systemjs/client/scripts/main.spa.ts
+++ code/src/systemjs/client/scripts/main.spa.ts
@@ -1,8 +1,10 @@
 /// <reference path="../../../../typings/index.d.ts" />
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {ROUTER_PROVIDERS} from '@angular/router';
+import {HTTP_PROVIDERS} from "@angular/http";
 import {myRouterComponent} from "./spa/myRouterComponent";
 
-bootstrap(myRouterComponent,[
-	ROUTER_PROVIDERS
+bootstrap(myRouterComponent, [
+	ROUTER_PROVIDERS,
+	HTTP_PROVIDERS
 ]);
--- code/src/systemjs/client/scripts/main.ts
+++ code/src/systemjs/client/scripts/main.ts
@@ -1,7 +1,10 @@
 /// <reference path="../../../../typings/index.d.ts" />
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {HomeComponent} from "../../../systemjs/client/scripts/home/HomeComponent"; //server side
+import {HTTP_PROVIDERS} from "@angular/http";
 
-bootstrap(HomeComponent);
+bootstrap(HomeComponent, [
+	HTTP_PROVIDERS
+]);
 
 
